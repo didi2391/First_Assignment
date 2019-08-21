0f8163548edd601d96110bc0c330d7883d56c9c7
didi2391 <dineshbaalakumar@gmail.com>
2019-08-21 13:06:52 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import "./UserInput";
+import UserInput from "./UserInput";
+import UserOutput from "./UserOutput";
 
 class App extends Component {
-  
+  render() {
+    const userName = "Aja";
+
+    return (
+      <div>
+        <UserInput />
+        <UserOutput userName={userName} />
+        <UserOutput userName={userName} />
+        <UserOutput userName={userName} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserOutput = props => {
+  return (
+    <div>
+      <p>{props.userName}</p>
+      <p>{props.userName}</p>
+    </div>
+  );
+};
+
+export default UserOutput;
